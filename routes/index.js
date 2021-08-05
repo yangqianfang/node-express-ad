01f54fd9c1864b5df11bcb46e79792454bdc0d0f
516010874 <516010874@qq.com>
2021-08-05 15:54:19 +0800

    'test'

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router()
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.send('hellow--test')
+    res.send('hellow--test--test2')
 })
 
 module.exports = router
