7158f617b747f63f05f8308147a1ba0acac4263d
jason <flyfreelyws@126.com>
2021-08-05 17:59:04 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router()
 /* GET home page. */
 router.get('/', function (req, res) {
     res.setHeader("Content-Type", "application/json;charset=utf-8")
-    res.send('{"code":0,"message":"permission denied","data":null}')
+    res.send('{"code":-1,"message":"permission denied","data":null}')
 })
 
 module.exports = router
