8968040ba6f3ffd96773d0d127483dc5893bd94b
516010874 <516010874@qq.com>
2021-08-06 10:30:50 +0800

    'fix'

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router()
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.setHeader("Content-Type", "application/json;charset=utf-8")
+    res.setHeader('Content-Type', 'application/json;charset=utf-8')
     res.send('{"code":-1,"message":"permission denied","data":null}')
 })
 
